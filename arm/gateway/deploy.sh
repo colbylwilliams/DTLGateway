ee5a3f98eccc333e466c1c498310c01faca00325
Colby Williams <ColbyLWilliams@gmail.com>
2021-02-24 23:12:38 -0600

    Update deploy.sh

--- arm/gateway/deploy.sh
+++ arm/gateway/deploy.sh
@@ -5,6 +5,12 @@
 
 set -e
 
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+die() { echo "${RED}Error: $1${NC}" >&2; exit 1; }
+
 cdir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 template="$cdir/azuredeploy.json"
@@ -35,20 +41,27 @@ Options:
 
   -k  The SSL certificate password for installation.
 
+  -i  Number of gateway scale set VM instances. default: 1
+
 Examples:
 
     $ deploy.sh -g MyResoruceGroup -l eastus -u DevUser -p SoSecure1 -c ./Cert.p12 -k 12345
 
 endHelp
 )
+helpText="${NC}$helpText${NC}\n"
 
 # show help text if called with no args
 if (($# == 0)); then
     echo "$helpText" >&2; exit 0
 fi
 
+# defaults
+instances=1
+sub=$( az account show | jq -r '.id' )
+
 # get arg values
-while getopts ":s:g:l:u:p:c:k:h:" opt; do
+while getopts ":hs:g:l:u:p:c:k:i:" opt; do
     case $opt in
         s)  sub=$OPTARG;;
         g)  rg=$OPTARG;;
@@ -57,34 +70,38 @@ while getopts ":s:g:l:u:p:c:k:h:" opt; do
         p)  adminPassword=$OPTARG;;
         c)  sslCert=$OPTARG;;
         k)  sslCertPassword=$OPTARG;;
+        i)  instances==$OPTARG;;
         h)  echo "$helpText" >&2; exit 0;;
-        \?) echo "    Invalid option -$OPTARG $helpText" >&2; exit 1;;
-        :)  echo "    Option -$OPTARG requires an argument $helpText." >&2; exit 1;;
+        \?) die "Invalid option -$OPTARG \n$helpText";;
+        :)  die "Option -$OPTARG requires an argument \n$helpText.";;
     esac
 done
 
-if [ ! -f "$sslCert" ]; then
-    echo "$sslCert not found.  Please check the path is correct and try again."; exit 1
-fi
-
-# check for the azure cli
-if ! [ -x "$(command -v az)" ]; then
-    echo "Error: az command is not installed.\n  The Azure CLI is required to run this deploy script.  Please install the Azure CLI, run az login, then try again.\n  Aborting." >&2; exit 1
-fi
-
-# check for jq
-if ! [ -x "$(command -v jq)" ]; then
-    echo "Error: jq command is not installed.\n  jq is required to run this deploy script.  Please install jq from https://stedolan.github.io/jq/download/, then try again.\n  Aborting." >&2; exit 1
-fi
-
 echo ""
 
-# TODO remove this after new version released 3/2
-function verCheck { printf "%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
-azversion=$( az version | jq -r '."azure-cli"' )
-if [ $(verCheck $azversion) -gt $(verCheck 2.18.0) ]; then
-    echo "There is a bug in az cli 2.19.0+ storage module that prevents this script from executing correctly.\n  Please downgrade to version 2.18.0 and run this script again" >&2; exit 1
-fi
+# check for jq
+[ -x "$(command -v jq)" ] || die "jq command is not installed.\njq is required to run this deploy script. Please install jq from https://stedolan.github.io/jq/download/, then try again."
+# check for the azure cli
+[ -x "$(command -v az)" ] || die "az command is not installed.\nThe Azure CLI is required to run this deploy script. Please install the Azure CLI, run az login, then try again."
+
+# temp fix start
+verCheck() { printf "%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+azversion=$( az version | jq -r '."azure-cli"' ) # TODO remove this after new version released 3/2
+[ $(verCheck $azversion) -gt $(verCheck 2.18.0) ] && die "There is a bug in az cli 2.19.0+ storage module that prevents this script from executing correctly. Please downgrade to version 2.18.0 and run this script again."
+# temp fix end
+
+
+[ ! -z "$sub" ] || die "-s must have a value\n$helpText"
+[ ! -z "$rg" ] || die "-g must have a value\n$helpText"
+[ ! -z "$region" ] || die "-l must have a value\n$helpText"
+[ ! -z "$adminUsername" ] || die "-u must have a value\n$helpText"
+[ ! -z "$adminPassword" ] || die "-p must have a value\n$helpText"
+[ ! -z "$sslCert" ] || die "-c must have a value\n$helpText"
+[ ! -z "$sslCertPassword" ] || die "-k must have a value\n$helpText"
+[ ! -z "$instances" ] || die "-i must have a value\n$helpText"
+
+# make sure sslCert is a path to a file
+[ -f "$sslCert" ] || die "-c $sslCert not found. Please check the path is correct and try again."
 
 
 # check if logged in to azure cli
@@ -109,9 +126,7 @@ deploy=$(az deployment group create --subscription $sub -g $rg \
                       sslCertificatePassword=$sslCertPassword \
                       sslCertificateThumbprint=$sslCertThumbprint | jq '.properties.outputs' )
 
-if [ -z "$deploy" ]; then
-  echo "Failed to deploy arm template - aborting." >&2; exit 1
-fi
+[ -z "$deploy" ] && "Failed to deploy arm template - aborting."
 
 
 if [ -d "$artifactsSource" ]; then
@@ -145,9 +160,6 @@ fi
 
 
 echo "\nDone."
-
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
 
 if [ ! -z "$sslCertCommonName" ]; then
   echo "\n\n${GREEN}Register Remote Desktop Gateway with your DNS using one of the following two options:${NC}\n"
